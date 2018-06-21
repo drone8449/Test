7b2d8091c92d896dc6f3d62af2477c23fecac173
leny <leny@172.30.1.1>
2018-06-21 18:54:12 +0900

    make_writer

--- src/main/java/edu/handong/csee/java/webcrawler/FileWriter.java
+++ src/main/java/edu/handong/csee/java/webcrawler/FileWriter.java
@@ -1,5 +1,28 @@
 package edu.handong.csee.java.webcrawler;
 
+import java.io.*;
+
 public class FileWriter {
 
+	String filename = "";
+	String output = "";
+	
+	public FileWriter(String filename, String output) {
+		this.filename = filename;
+		this.output = output;
+	}
+	
+	public void getOutput() {
+		
+		PrintWriter writer = null;
+
+		try {
+			writer = new PrintWriter(filename);
+			writer.write(output + "\n");
+		} catch(FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		writer.close();
+	}
 }
