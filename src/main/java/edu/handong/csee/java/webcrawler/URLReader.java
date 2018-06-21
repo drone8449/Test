6552e97c3582756b964ab2a7d4a25c7a4f167263
leny <leny@172.30.1.1>
2018-06-21 18:45:11 +0900

    make_reader

--- src/main/java/edu/handong/csee/java/webcrawler/URLReader.java
+++ src/main/java/edu/handong/csee/java/webcrawler/URLReader.java
@@ -1,5 +1,30 @@
 package edu.handong.csee.java.webcrawler;
 
-public class URLReader {
+import java.io.*;
+import java.net.*;
 
+public class URLReader {
+	
+	String path = "";
+	String output = "";
+
+	public URLReader(String path) {
+		this.path = path;
+	}
+
+	public String getResult() throws Exception{
+
+		URL address = new URL(path);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(address.openStream()));
+
+		String input = "";
+		
+		while ((input = reader.readLine()) != null) {
+			output = output+input;
+		}
+		
+		reader.close();
+
+		return output;
+	}
 }
